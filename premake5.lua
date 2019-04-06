cdb81e6091c793d2d64a2190fa262f5ddcfb4d02
Martin Halfar <HafisCZ@users.noreply.github.com>
2019-04-06 15:29:03 +0200

    1.0.0 - Added glm

--- premake5.lua
+++ premake5.lua
@@ -15,6 +15,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Haze/vendor/GLFW/include"
 IncludeDir["Glad"] = "Haze/vendor/Glad/include"
 IncludeDir["ImGui"] = "Haze/vendor/imgui"
+IncludeDir["glm"] = "Haze/vendor/glm"
 
 group "Dependencies"
 	include "Haze/vendor/GLFW"
@@ -37,7 +38,9 @@ project "Haze"
 	files 
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs 
@@ -46,7 +49,8 @@ project "Haze"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -106,7 +110,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Haze/vendor/spdlog/include",
-		"Haze/src"
+		"Haze/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
